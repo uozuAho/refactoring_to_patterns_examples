90e3c371297e7500b24ed8f749c40736f5a5e28f
Warwick Stone <uozu.aho@gmail.com>
2020-10-17 20:52:54 +1100

    finish composition refactoring

--- polymorphic_creation_with_factory/refactored_with_composition/OutputBuilderTests.cs
+++ polymorphic_creation_with_factory/refactored_with_composition/OutputBuilderTests.cs
@@ -1,11 +1,27 @@
 using System;
+using NUnit.Framework;
 
 namespace polymorphic_creation_with_factory.refactored_with_composition
 {
-    public class XmlBuilderTest : TestCase
+    [TestFixture(typeof(DomBuilder))]
+    [TestFixture(typeof(XmlBuilder))]
+    public class OutputBuilderTests
     {
         private OutputBuilder _builder;
+        private readonly Type _builderType;
 
+        public OutputBuilderTests(Type builderType)
+        {
+            _builderType = builderType;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _builder = (OutputBuilder) Activator.CreateInstance(_builderType, "orders");
+        }
+
+        [Test]
         public void TestAddAboveRoot()
         {
             var invalidResult = "<orders>" +
@@ -14,12 +30,11 @@ namespace polymorphic_creation_with_factory.refactored_with_composition
                                 "</orders>" +
                                 "<customer>" +
                                 "</customer>";
-            _builder = new XmlBuilder("orders");
             _builder.AddBelow("order");
             try
             {
                 _builder.AddAbove("customer");
-                Fail("expecting Exception");
+                Assert.Fail("expecting Exception");
             }
             catch (Exception)
             {
