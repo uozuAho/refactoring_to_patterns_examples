9328a01ca1d3db5265d2683f8f12f77ecc10fbcb
Warwick Stone <uozu.aho@gmail.com>
2020-10-17 18:26:11 +1100

    pull up similar method

--- polymorphic_creation_with_factory/refactored/XmlBuilderTest.cs
+++ polymorphic_creation_with_factory/refactored/XmlBuilderTest.cs
@@ -1,30 +1,7 @@
-using System;
-
 namespace polymorphic_creation_with_factory.refactored
 {
     public class XmlBuilderTest : OutputBuilderTestBase
     {
-        public void TestAddAboveRoot()
-        {
-            var invalidResult = "<orders>" +
-                                "<order>" +
-                                "</order>" +
-                                "</orders>" +
-                                "<customer>" +
-                                "</customer>";
-            Builder = CreateBuilder("orders");
-            Builder.AddBelow("order");
-            try
-            {
-                Builder.AddAbove("customer");
-                Fail("expecting Exception");
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-        }
-
         protected override OutputBuilder CreateBuilder(string rootName)
         {
             return new XmlBuilder(rootName);
