11abce865de561520e30978648a3ce8ab1ed72d3
Warwick Stone <uozu.aho@gmail.com>
2020-11-02 15:00:11 +1100

    init

---
+++ replace_conditional_with_strategy/initial_state/UnusedRiskFactor.cs
@@ -0,0 +1,15 @@
+﻿namespace replace_conditional_with_strategy.initial_state
+{
+    internal class UnusedRiskFactor
+    {
+        public static UnusedRiskFactor GetFactors()
+        {
+            return new UnusedRiskFactor();
+        }
+
+        public double ForRating(double riskRating)
+        {
+            return 4;
+        }
+    }
+}
