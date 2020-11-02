7741464d0aadcb4ab4985422645595820dd2fdc7
Warwick Stone <uozu.aho@gmail.com>
2020-11-02 17:18:23 +1100

    make Capital abstract, move duration

--- replace_conditional_with_strategy/refactored/CapitalStrategy.cs
+++ replace_conditional_with_strategy/refactored/CapitalStrategy.cs
@@ -2,31 +2,16 @@ using System;
 
 namespace replace_conditional_with_strategy.refactored
 {
-    public class CapitalStrategy
+    public abstract class CapitalStrategy
     {
         private const int MillisPerDay = 3600 * 24 * 1000;
         private const int DaysPerYear = 360;
 
-        public virtual double Capital(Loan loan)
-        {
-            if (loan.Expiry == null && loan.Maturity != null)
-                return new CapitalStrategyTermLoan().Capital(loan);
-            if (loan.Expiry != null && loan.Maturity == null)
-            {
-                if (loan.GetUnusedPercentage() != 1.0)
-                    return new CapitalStrategyRevolver().Capital(loan);
-                else
-                    return new CapitalStrategyAdvisedLine().Capital(loan);
-            }
+        public abstract double Capital(Loan loan);
 
-            return 0.0;
-        }
-
-        public double Duration(Loan loan)
+        public virtual double Duration(Loan loan)
         {
-            if (loan.Expiry == null && loan.Maturity != null) return WeightedAverageDuration(loan);
-            else if (loan.Expiry != null && loan.Maturity == null) return YearsTo(loan.Expiry.Value);
-            return 0.0;
+            return YearsTo(loan.Expiry.Value);
         }
 
         protected double RiskFactor(double riskRating)
@@ -39,22 +24,7 @@ namespace replace_conditional_with_strategy.refactored
             return refactored.UnusedRiskFactor.GetFactors().ForRating(riskRating);
         }
 
-        private double WeightedAverageDuration(Loan loan)
-        {
-            var duration = 0.0;
-            var weightedAverage = 0.0;
-            var sumOfPayments = 0.0;
-            foreach (var payment in loan.Payments)
-            {
-                sumOfPayments += payment.Amount();
-                weightedAverage += YearsTo(payment.Date()) * payment.Amount();
-            }
-
-            if (loan.Commitment != 0.0) duration = weightedAverage / sumOfPayments;
-            return duration;
-        }
-
-        private double YearsTo(DateTime endDate)
+        protected double YearsTo(DateTime endDate)
         {
             var beginDate = DateTime.Now;
             return ((endDate - beginDate).Milliseconds / MillisPerDay) / DaysPerYear;
--- replace_conditional_with_strategy/refactored/CapitalStrategyTermLoan.cs
+++ replace_conditional_with_strategy/refactored/CapitalStrategyTermLoan.cs
@@ -6,5 +6,25 @@ namespace replace_conditional_with_strategy.refactored
         {
             return loan.Commitment * Duration(loan) * RiskFactor(loan.RiskRating);
         }
+
+        public override double Duration(Loan loan)
+        {
+            return WeightedAverageDuration(loan);
+        }
+
+        private double WeightedAverageDuration(Loan loan)
+        {
+            var duration = 0.0;
+            var weightedAverage = 0.0;
+            var sumOfPayments = 0.0;
+            foreach (var payment in loan.Payments)
+            {
+                sumOfPayments += payment.Amount();
+                weightedAverage += YearsTo(payment.Date()) * payment.Amount();
+            }
+
+            if (loan.Commitment != 0.0) duration = weightedAverage / sumOfPayments;
+            return duration;
+        }
     }
 }
