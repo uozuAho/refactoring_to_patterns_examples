dc02f224d7bc9175327a60101cdfed870d87c9af
Warwick Stone <uozu.aho@gmail.com>
2020-11-07 15:10:20 +1100

    add StringNode.Text to initial state

--- replace_embellishment_with_decorator/initial_state_two/StringNode.cs
+++ replace_embellishment_with_decorator/initial_state_two/StringNode.cs
@@ -2,6 +2,8 @@ namespace replace_embellishment_with_decorator.initial_state_two
 {
     internal class StringNode : AbstractNode
     {
+        public string Text { get; set; }
+
         private readonly string _textBuffer;
         private readonly bool _shouldDecodeNodes;
         private readonly bool _shouldRemoveEscapeCharacters;
