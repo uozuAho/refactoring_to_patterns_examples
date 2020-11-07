745ee27bb50ad4010d3fde0c61a098607f2ba971
Warwick Stone <uozu.aho@gmail.com>
2020-11-07 15:08:13 +1100

    add to inode

--- replace_embellishment_with_decorator/refactored/INode.cs
+++ replace_embellishment_with_decorator/refactored/INode.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
+
 namespace replace_embellishment_with_decorator.refactored
 {
     internal interface INode
     {
         string ToPlainTextString();
         string ToHtml();
+        void CollectInto(List<INode> nodes, string filter);
+        void CollectInto(List<INode> nodes, Type nodeType);
+        void Accept(NodeVisitor visitor);
     }
 }
